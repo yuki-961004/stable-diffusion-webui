ef4e42b6b3adedc3d2cd0f820fad13ef24d46e0f
yuki-961004 <hmz1969a@gmail.com>
2023-07-07 16:20:14 +0800

    my setting

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON="D:\ProgramData\Python\Python-3.10.6\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--xformers
 
 call webui.bat
